935c5b65ac779576cfeb5d5ccab360b539d1ec0c
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-04-14 13:00:19 -0700

    Adding the underline transition effect to navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Logo from "./Logo";
 
 const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
   return (
-    <Link href={href} className={`${className}`}>
+    <Link href={href} className={`${className} relative group`}>
       {title}
+
+      <span
+        className={`
+        h-[2px] inline-block bg-dark
+        absolute left-0 -bottom-0.5 group-hover:w-full 
+        transition-[width] ease duration-300
+        ${router.asPath === href ? "w-full" : "w-0"}
+        `}
+      >
+        &nbsp;
+      </span>
     </Link>
   );
 };
