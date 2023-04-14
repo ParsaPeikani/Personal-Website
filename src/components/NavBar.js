d3021241f53c753e1fc7f17e04309b0884f9ac14
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-04-13 22:43:10 -0700

    Styling the navbar component as well as the logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,23 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
+const CustomLink = ({ href, title, className = "" }) => {
+  return (
+    <Link href={href} className={`${className}`}>
+      {title}
+    </Link>
+  );
+};
+
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
       </nav>
-      <Logo />
+
       <nav>
         <Link href="/" targe={"_blank"}>
           T
@@ -30,6 +38,10 @@ const NavBar = () => {
           T
         </Link>
       </nav>
+
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
   );
 };
