3ce178082822ce3e3dc4ce09d28e3949643cb7f9
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-04-27 12:50:17 -0700

    Created the about page without skill section

---
+++ src/pages/about.js
@@ -0,0 +1,84 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import profilePic from "../../public/images/profile/parsa.png";
+import Image from "next/image";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Parsa Peikani | About Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text="Passion Fuels Purpose! " className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi, I'm Parsa Peikani, a web developer and backend designer with
+                a passion for creating beautiful, functional, and user-centered
+                digital experiences. With 1 years of experience in Linux with
+                panada Hausi. I am always looking for new and innovative ways to
+                bring my clients' visions to life becuase it is "VERY
+                IMPORTANT!!".
+              </p>
+              <p className="my-4 font-medium">
+                I am grinding leetcode by solving 10-20 medium to hard questions
+                everyday. I know I can push my limits by solving even more
+                questions.
+              </p>
+              <p className="font-medium">
+                Currently I am busy working on a million dollar project where
+                you can submit a page and I'll give you the texts in the
+                submitted image. So if you are looking forward to hire me, ...
+                Nah, I'm good!!
+              </p>
+            </div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image
+                src={profilePic}
+                alt="Parsa Peikani"
+                className="w-full h-auto rounded-2xl"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold hover:animate-rubberBand hover:text-red-500 cursor-pointer">
+                  50+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  satisfied clients
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold hover:animate-rubberBand hover:text-red-500 cursor-pointer">
+                  40+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  projects completed
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold hover:animate-rubberBand hover:text-red-500 cursor-pointer">
+                  4+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  years of experience
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+export default about;
