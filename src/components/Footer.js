259cac19af023d01eb9319c7b42b210fbfbb08e9
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-05-04 15:46:41 -0700

    Added the earth three3d component to the right of the contact section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import emailjs from "emailjs-com";
 import Link from "next/link";
 import AnimatedText from "./AnimatedText";
+import Earth from "./canvas/Earth";
 
 const Contact = () => {
   const formRef = useRef();
@@ -16,7 +17,7 @@ const Contact = () => {
   const handleChange = (e) => {};
   const handleSubmit = (e) => {};
   return (
-    <div className="">
+    <div>
       <h1 className="dark:text-yellow-400/75 text-3xl text-center">
         Get In Touch
       </h1>
@@ -24,55 +25,61 @@ const Contact = () => {
         text="Contact."
         className="text-yellow-400 text-center text-8xl font-bold"
       />
-
-      <motion.div className="flex-[0.75] bg-black-100 p-8 rounded-2xl w-1/2">
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
-          <label className=" pl-20 flex flex-col">
-            <span className="text-yellow-400 font-bold mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className="bg-purple-950 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="pl-20 flex flex-col">
-            <span className="text-yellow-400 font-bold mb-4">Your email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className="bg-purple-950  py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="pl-20 flex flex-col">
-            <span className="text-yellow-400 font-bold mb-4">Your Message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What you want to say?"
-              className="bg-purple-950  py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-
-          <button
-            type="submit"
-            className="w-fit bg-dark dark:bg-yellow-400 text-light dark:text-dark p-2.5 px-6 rounded-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark hover:dark:bg-dark mx-auto hover:dark:border-yellow-400 hover:dark:text-yellow-400"
+      <div className="flex">
+        <motion.div className="flex-[0.75] bg-black-100 p-8 rounded-2xl w-1/2">
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+            className="mt-12 flex flex-col gap-8"
           >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </motion.div>
+            <label className=" pl-20 flex flex-col">
+              <span className="text-yellow-400 font-bold mb-4">Your Name</span>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+                placeholder="What's your good name?"
+                className="bg-purple-950 py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+            <label className="pl-20 flex flex-col">
+              <span className="text-yellow-400 font-bold mb-4">Your email</span>
+              <input
+                type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="What's your web address?"
+                className="bg-purple-950  py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+            <label className="pl-20 flex flex-col">
+              <span className="text-yellow-400 font-bold mb-4">
+                Your Message
+              </span>
+              <textarea
+                rows={7}
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+                placeholder="What you want to say?"
+                className="bg-purple-950  py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              />
+            </label>
+
+            <button
+              type="submit"
+              className="w-fit bg-dark dark:bg-yellow-400 text-light dark:text-dark p-2.5 px-6 rounded-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark hover:dark:bg-dark mx-auto hover:dark:border-yellow-400 hover:dark:text-yellow-400"
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+          </form>
+        </motion.div>
+        <motion.div className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] w-1/2">
+          <Earth />
+        </motion.div>
+      </div>
     </div>
   );
 };
